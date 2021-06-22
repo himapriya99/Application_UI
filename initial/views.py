6bacc6645c40a24796904872da87933e42f7b53e
Tammineedi <Himapriya_Tammineedi@Dell.com>
2021-06-22 13:19:27 +0530

    alerts

--- initial/views.py
+++ initial/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 # Create your views here.
-
+data = {}
+data['message'] = None
 def input (request):  
     return render( request, "initial/input.html")
 
 def submit(request):
-    data = {}
+    #data = {}
     table=[]# get data into table variable from models ex: table = <Model_name>.objects.all()
     if request.method == "POST":
         release = str(request.POST.get('Release'))
@@ -17,16 +20,20 @@ def submit(request):
         print(release)
         print(lock)
         #getting all stories from tfs using release into DB
-    if lock == "scopelock":
+    if data['lock'] == "scopelock":
        return render(request, "initial/scopel.html", data)
-    elif lock == 'codelock':
+    elif data['lock'] == 'codelock':
        return render( request, "initial/codel.html",data)
     else :
        return render( request, "initial/input.html")
 
 def scope_alert(request):
     #validations and email code for scope lock
-    return HttpResponse('scopelock')#popup page
+   # return HttpResponse('scopelock')#popup page
+   
+     #return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+     #return redirect(request,"initial/scopel.html",context)
+     return render(request,"initial/pop.html",data)
 
 def code_alert(request):
     #validations and email code for code lock
